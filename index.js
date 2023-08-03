4b955560150e0c266d909663c8c91411e07a801b
unknown <reman1999@outlook.com>
2023-08-03 19:45:14 +0200

    test commit for CORS

--- index.js
+++ index.js
@@ -29,28 +29,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = [
-  'http://localhost:3000',
-  'http://testsite.com',
-  'http://localhost:1234',
-  'http://127.0.0.1:1234',
-];
+// let allowedOrigins = [
+//   'http://localhost:3000',
+//   'http://testsite.com',
+//   'http://localhost:1234',
+//   'http://127.0.0.1:1234',
+// ];
 app.use(cors());
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require('./auth')(app);
 const passport = require('passport');
